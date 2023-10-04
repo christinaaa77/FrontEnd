170a34c017a898616a00205a14639465ce38f8e8
Christina Golung <christinagolung77@gmail.com>
2023-10-04 08:10:37 +0800

    Materi Frond End

---
+++ pertemuan15/index.js
@@ -0,0 +1,9 @@
+//BASIC GIT AND GITHUB
+
+//Git merupakan tool utama yang pasti akan anda gunakan setiap hari, dan juga sepanjang karir anda sebagai developer. 
+//Dengan ini anda dapat tahu apa saja yang anda lakukan setiap saat dan berkolaborasi dengan rekan lainnya juga. 
+//Anggaplah Git sebagai mesin waktu untuk berbagai pekerjaan dan file/folder yang kamu kelola. 
+//Sedangkan GitHub merupakan tempat sekaligus komunitas untuk para developer berbagai code dan berkolaborasi dalam berbagai project software. 
+//Bahkan modern ini, profil GitHub dapat menggantikan resume/CV untuk melamar pekerjaan.
+//Saat ini anda akan melakukan instalasi dan konfigurasi Git di komputer anda, mampu menjelaskan dan membedakan Git dan GitHub, 
+//serta memahami manfaat dan kekuatan version control system (VCS) atau nama lainnya source code management (SCM).
